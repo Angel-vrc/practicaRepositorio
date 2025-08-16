684c32a740acfcc529a17710129b05a823ae28bd
angelica <angelicavargascancimanse@gmail.com>
2025-08-16 14:23:35 -0500

    Prueba commit NetBeans

--- src/pruebagit/PruebaGit.java
+++ src/pruebagit/PruebaGit.java
@@ -14,7 +14,7 @@ public class PruebaGit {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hola GitHub");
+        System.out.println("Hola GitHub?????");
     }
     
 }
